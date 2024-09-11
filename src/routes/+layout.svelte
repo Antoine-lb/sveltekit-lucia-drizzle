9772255e6e084726b00e2de8ac044d6c36c79f06
Antoine LB <antoine.stc25@gmail.com>
2024-09-11 17:45:26 -0300

    add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <!-- routes/+page.svelte -->
 <script lang="ts">
 	import { enhance } from '$app/forms';
+
+	import '../app.css';
 </script>
 
-<nav>
+<nav class="flex flex-row-reverse bg-blue-900 gap-2 text-white px-4 py-2">
 	<a href="/signup">Signup</a>
 	<a href="/login">Login</a>
 	<form method="post" use:enhance>
